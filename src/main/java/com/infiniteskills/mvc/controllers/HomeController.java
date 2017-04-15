fd6ba845a19e0406f887964f4d9e996de64fd58c
krishna <krishna@krishna-PC>
2017-04-15 19:01:41 +0530

    0402- Controllers Working
    views configured(project_add.jsp)
    web.xml welcome page done

--- src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
+++ src/main/java/com/infiniteskills/mvc/controllers/HomeController.java
@@ -1,8 +1,10 @@
 package com.infiniteskills.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.infiniteskills.mvc.data.entities.Project;
 
 @Controller
 public class HomeController {
@@ -10,6 +12,21 @@ public class HomeController {
 	@RequestMapping("/home")
 //	@ResponseBody
 	public String goHome(){
+		return "home";
+	}
+	
+	
+	
+	@RequestMapping("/")
+	public String goHome(Model model){
+		
+		Project project = new Project();
+		project.setName("First Project");
+		project.setSponsor("Nasa");
+		project.setDescription("This is a simple project sponsored by NASA");
+		
+		model.addAttribute("currentProject", project);
+		
 		return "home";
 	}
 }
